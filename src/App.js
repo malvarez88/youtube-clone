9d8f3d6577b44cffd2cd1585a789436a4d55a234
Malvarez88 <marianoalvarez66@gmail.com>
2022-08-28 21:51:28 -0300

    constants

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Box } from '@mui/material';
-import Navbar from './components/Navbar';
-import Feed from './components/Feed';
-import VideoDetail from './components/VideoDetail';
-import ChannelDetail from './components/ChannelDetail';
-import SearchFeed from './components/SearchFeed';
+
+import { Navbar, Feed, VideoDetail, ChannelDetail, SearchFeed} from './components';
 
 const App = () => 
   (
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,6 @@
-import React from 'react'
+import { Stack } from '@mui/material';
+import { Link } from 'react-router-dom';
+
 
 const Navbar = () => {
   return (
