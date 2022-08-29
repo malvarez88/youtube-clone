abdb2ca53bb8909152c162a478f2daf4f6771426
Malvarez88 <marianoalvarez66@gmail.com>
2022-08-29 15:36:01 -0300

    fetch ok

--- src/utils/fetchFromAPI.js
+++ src/utils/fetchFromAPI.js
@@ -1,24 +1,20 @@
 import axios from 'axios';
 
-const BASE_URL = 'https://youtube138.p.rapidapi.com';
+export const BASE_URL = 'https://youtube-v31.p.rapidapi.com';
 
 const options = {
-    url: BASE_URL,
-    params: {q: 'despacito', hl: 'en', gl: 'US'},
+    params: {
+      maxResults: '50'
+    },
     headers: {
       'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
-      'X-RapidAPI-Host': 'youtube138.p.rapidapi.com'
+      'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com'
     }
   };
   
-//   axios.request(options).then(function (response) {
-//       console.log(response.data);
-//   }).catch(function (error) {
-//       console.error(error);
-//   });
 
-  export const fetchFromAPI = async (url) => {
-    const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+export const fetchFromAPI = async (url) => {
+  const { data } = await axios.get(`${BASE_URL}/${url}`, options);
 
-    return data;
-  }
+  return data;
+};
