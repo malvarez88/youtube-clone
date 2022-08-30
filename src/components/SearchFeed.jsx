1ff58f7b0af29fe2a53e1b39f02ae6b6e899e82f
Malvarez88 <marianoalvarez66@gmail.com>
2022-08-29 21:55:03 -0300

    searchfeed & videodetail

--- src/components/SearchFeed.jsx
+++ src/components/SearchFeed.jsx
@@ -16,11 +16,14 @@ const SearchFeed = () => {
   }, [searchTerm]);
 
   return (
-    <Box p={2} sx={{ overflowY: "auto", height: "90vh", flex: 2 }}>
-      <Typography variant="h4" fontWeight="bold" mb={2} sx={{ color: "white" }}>
-        Search Results for: <span style={{ color: "#FC1503" }}>{searchTerm}</span> Videos
+    <Box p={2} minHeight="95vh">
+      <Typography variant="h4" fontWeight={900}  color="white" mb={3} ml={{ sm: "100px"}}>
+        Search Results for <span style={{ color: "#FC1503" }}>{searchTerm}</span> videos
       </Typography>
-      <Videos videos={videos} />
+      <Box display="flex">
+        <Box sx={{ mr: { sm: '100px' } }}/>
+        {<Videos videos={videos} />}
+      </Box>
     </Box>
   );
 };
